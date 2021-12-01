4674ace15b2dd17121ebacf9e6319159519b1fed
nocilOLyus <olyusroriconmaster@gmail.com>
2021-12-01 10:55:54 +0100

    improved day1.py a lil bit by removing a variable

--- day1/day1.py
+++ day1/day1.py
@@ -6,23 +6,23 @@ with open("input.txt", "r") as f:
     previous_depth = int(measurements[0])
     increased = 0
     sums = []
-    sum_increased = 0
 
     # Part 1
     for depth in measurements:
         if int(depth) > previous_depth:
             increased += 1
         previous_depth = int(depth)
+    print(increased)
 
     # Part 2
+    increased = 0
     for start in range(len(measurements) - 2):
         sum = int(measurements[start]) + int(measurements[start + 1]) + int(measurements[start + 2])
         sums.append(sum)
         if len(sums) == 2:
             if sums[1] > sums[0]:
-                sum_increased += 1
+                increased += 1
             sums[0] = sums[1]
             sums.pop()
 
     print(increased)
-    print(sum_increased)
