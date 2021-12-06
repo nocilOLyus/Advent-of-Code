2312a79cb92e3c791f7ecb74c444186e10b463c2
nocilOLyus <olyusroriconmaster@gmail.com>
2021-12-06 15:45:10 +0100

    day5 and day6 python

---
+++ day5/day5.py
@@ -0,0 +1,87 @@
+with open("input.txt", "r") as f:
+    data = f.readlines()
+    data = [_[:-1] for _ in data]
+    lines = [_.split(" -> ") for _ in data]
+
+points = [[] for list in lines]
+for l_ind, line in enumerate(lines):
+    for point in line:
+        points[l_ind].append(point.split(","))
+
+#print(points)
+
+def get_max_coords(points):
+    maxX = maxY = 0
+    for line in points:
+        for point in line:
+            if int(point[0]) > maxX: maxX = int(point[0])
+            if int(point[1]) > maxY: maxY = int(point[1])
+    return (maxX, maxY)
+
+max_coords = get_max_coords(points)
+
+#def part1(lines):
+#    # [[['0', '9'], ['5', '9']]]
+#    max_coords = get_max_coords(lines)
+#    overlap = 0
+#    for y in range(max_coords[1] + 1):
+#        for x in range(max_coords[0] + 1):
+#            value = 0
+#            for line in points:
+#                startX = min(int(line[0][0]), int(line[1][0]))
+#                startY = min(int(line[0][1]), int(line[1][1]))
+#                endX = max(int(line[0][0]), int(line[1][0]))
+#                endY = max(int(line[0][1]), int(line[1][1]))
+#                if startX <= x <= endX and startY == y == endY: value += 1
+#                if startX == x == endX and startY <= y <= endY: value += 1
+#            if value != 0: print(value, end="")
+#            else: print(".", end="")
+#            if value >= 2: overlap += 1
+#        print()
+#    return overlap
+
+
+def solve(lines, diagonal = False, draw = False):
+    coords = []
+    for i in range((max_coords[0] + 1) * (max_coords[1] + 1)):
+        coords.append(0)
+
+    for line in points:
+        if int(line[0][0]) == int(line[1][0]):
+            startY = min(int(line[0][1]), int(line[1][1]))
+            endY = max(int(line[0][1]), int(line[1][1])) + 1
+            for p in range(startY, endY):
+                coords[int(line[0][0]) + (p * (max_coords[0] + 1))] += 1
+        elif int(line[0][1]) == int(line[1][1]):
+            startX = min(int(line[0][0]), int(line[1][0]))
+            endX = max(int(line[0][0]), int(line[1][0])) + 1
+            for p in range(startX, endX):
+                coords[p + (int(line[0][1]) * (max_coords[0] + 1))] += 1
+        elif diagonal:
+            if int(line[0][0]) < int(line[1][0]):
+                x_increment = 1
+            else: x_increment = -1
+            if int(line[0][1]) < int(line[1][1]):
+                y_increment = 1
+            else: y_increment = -1
+            for p in range(abs(int(line[0][0]) - int(line[1][0])) + 1):
+                if p == 0:
+                    coords[int(line[0][0]) + (int(line[0][1]) * (max_coords[0] + 1))] += 1
+                elif p > 0:
+                    coords[(int(line[0][0]) + (p * x_increment)) + ((int(line[0][1]) + (p * y_increment)) * (max_coords[0] + 1))] += 1
+            
+    overlaps = 0
+    to_write = ""
+    for index, coord in enumerate(coords):
+        if coord >= 2: overlaps += 1
+        if draw:
+            if coord == 0: to_write += "."
+            else: to_write += str(coord)
+            if index % max_coords[0] == 0 and index != 0 and index < max_coords[0] + 1: to_write += "\n"
+            elif (index + 1) % (max_coords[0] + 1) == 0: to_write += "\n"
+    print(to_write, "\n")
+    return overlaps
+
+print(f"\nPart 1: {solve(points, draw = False)}")
+
+print(f"\nPart 2: {solve(points, diagonal = True, draw = False)}")
---
+++ day6/day6.py
@@ -0,0 +1,26 @@
+from collections import deque           # to rotate list
+
+with open("input.txt", "r") as f:
+    state_str = f.read().split(",")
+    state = [int(fish) for fish in state_str]
+
+nb_fish = deque([])
+for i in range(9):
+    nb_fish.append(0)
+    nb_fish[i] = state.count(i)
+
+def update(school, day = 1):
+    global nb_fish
+    while day >= 1:
+        nb_fish.rotate(-1)
+        nb_fish[6] += nb_fish[8]
+        day -= 1
+
+def solve(school):
+    total = 0
+    for cell in school:
+        total += cell
+    return total
+
+update(nb_fish, 256)
+print(f"Number of fish: {solve(nb_fish)}")
